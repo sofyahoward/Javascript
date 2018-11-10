ae0b6cf1f5ab48d50861498cb2ef4f0a97b984aa
sofyahoward <sofya.howard@gmail.com>
2018-11-10 16:05:37 -0300

    added the Farm Problem

---
+++ TheFarmProblem.js
@@ -0,0 +1,13 @@
+// You've got chickens (2 legs), cows (4 legs) and pigs (4 legs) on your farm. Return the total number of legs on your farm.
+
+// Examples
+// animals(2, 3, 5) ➞ 36
+
+// animals(1, 2, 3) ➞ 22
+
+// animals(5, 2, 8) ➞ 50
+
+function animals(chickens, cows, pigs) {
+    return chickens * 2 + cows * 4 + pigs * 4
+}
+
--- a Very Large Sum.js
+++
@@ -1,6 +0,0 @@
-//find a sum of an array where n is the nuber of items in an array and ar is the array of integers
-var aVeryBigSum = (n,ar) => {
-  var sum = ar.reduce((a,b) => a+b);
-  return sum;
-};
-aVeryBigSum(2, [1, 2]);
