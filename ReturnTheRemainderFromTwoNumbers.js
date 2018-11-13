f92efd4afff7431a1194266d2a60464916063bf6
sofyahoward <sofya.howard@gmail.com>
2018-11-13 16:31:24 -0300

    Get the ramainder of two numbers

---
+++ ReturnTheRemainderFromTwoNumbers.js
@@ -0,0 +1,12 @@
+// There is a single operator in JavaScript capable of providing the remainder of a division operation.Two numbers are passed as parameters.The first provider divided by the second parameter will have a remainder, possiby zero.Return that value.
+
+// Examples
+// remainder(1, 3) ➞ 1
+
+// remainder(-9, 45) ➞ -9
+
+// remainder(5, 5) ➞ 0
+
+function remainder(x, y) {
+    return x % y
+}
